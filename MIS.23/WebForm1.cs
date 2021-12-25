0b0a882bc8dba784ce3ba5ac8fb8cab7db6db38a
shadaelhakeim <90161184+shadaelhakeim@users.noreply.github.com>
2021-12-25 23:20:04 +0200

    Create WebForm1.cs

---
+++ MIS.23/WebForm1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+
+
+namespace MIS._23
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            OleDbConnection f = new OleDbConnection(ConfigurationManager.ConnectionStrings["test"].ConnectionString);
+            OleDbCommand cmd = new OleDbCommand("insert into [user]([username],[password],[conpassword],[email])values ('" + name.Text + "','" + password.Text + "','" + conpassword.Text + "','" + email.Text + "')", connection: f);
+            f.Open();
+            cmd.ExecuteNonQuery();
+            f.Close();
+        }
+    }
+}
